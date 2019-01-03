4128f9e2d0cc6e7e07e7db6b4caf3f58dacdd184
Luke Allen <nightwolf55la@gmail.com>
2019-01-02 18:39:27 -0600

    Adding comments to generator.h and generator_iterator.h

--- include/util/gen/generator.h
+++ include/util/gen/generator.h
@@ -3,20 +3,44 @@
 #include "generator_iterator.h"
 
 namespace util::gen {
+    ////////////////////////////////////////////////////////////////////////////////
+    // Generator - This class encapsulates the common code of generator style
+    // classes and enforces a common structure / behavior.
+    ////////////////////////////////////////////////////////////////////////////////
     template<class Impl>
     class Generator : public Impl {
     private:
+        //------------------------------------------------------------------------------
+        // Types
+        //------------------------------------------------------------------------------
         using Iterator = GeneratorIterator<Generator>;
 
     public:
+        //------------------------------------------------------------------------------
+        // begin/end - standard iterator access and enable use in ranged-for loops
+        //------------------------------------------------------------------------------
         constexpr Iterator begin() { return Iterator(*this); }
         constexpr GeneratorEnd end() { return {}; }
 
+        //------------------------------------------------------------------------------
+        // operators
+        // operator* - This should return the current state of the generator
+        // operator++ - This should advance the generator. Note only the prefix++ is provided.
+        //              Due to the generator having one state and not being copyable
+        //              a postfix++ doesn't make sense and would only provide behavior
+        //              that is inconsistent with a regular postfix++ operator.
+        // operator bool - This should return true while the generator can still be advanced.
+        //                 Once the generator should not be advanced anymore, this should return false
+        //------------------------------------------------------------------------------
         constexpr decltype(auto) operator*() { return Impl::operator*(); }
         constexpr void operator++() { Impl::operator++(); } // TODO: Should this return a Generator& (*this)
         constexpr explicit operator bool() const { return Impl::operator bool(); }
 
     public:
+        //------------------------------------------------------------------------------
+        // Implicit Constructors/assignment - All deleted
+        // Due to the nature of a generator, moving or copying doesn't make sense
+        //------------------------------------------------------------------------------
         constexpr Generator(const Generator&) = delete;
         constexpr Generator(Generator&&) = delete;
         constexpr Generator& operator=(const Generator&) = delete;
--- include/util/gen/generator_iterator.h
+++ include/util/gen/generator_iterator.h
@@ -1,11 +1,23 @@
 #pragma once
 
 namespace util::gen {
+    ////////////////////////////////////////////////////////////////////////////////
+    // GeneratorEnd - This serves as a sentinel for when a generator is finished
+    ////////////////////////////////////////////////////////////////////////////////
     struct GeneratorEnd {};
 
+    ////////////////////////////////////////////////////////////////////////////////
+    // GeneratorIterator - This is common iterator behavior used by generators
+    ////////////////////////////////////////////////////////////////////////////////
     template<class Generator>
     class GeneratorIterator {
     public:
+        //------------------------------------------------------------------------------
+        // Constructors - All implicit construction except default is allowed
+        //              - Must be constructed with a Generator reference
+        //              - This keeps the iterator small, which isn't a requirement, but
+        //                it seems to be an expectation at this point
+        //------------------------------------------------------------------------------
         constexpr explicit GeneratorIterator(Generator& generator) : generator_(generator) { }
         constexpr GeneratorIterator(const GeneratorIterator&) = default;
         constexpr GeneratorIterator(GeneratorIterator&&) noexcept = default;
@@ -13,11 +25,20 @@ namespace util::gen {
         constexpr GeneratorIterator& operator=(GeneratorIterator&&) noexcept = default;
 
     public:
+        //------------------------------------------------------------------------------
+        // operators
+        // operator* - Calls the generator operator*
+        // operator++ - Calls the generator operator++, but returns itself
+        // operator!= - Only defined for the Generator sentinel, calls the generators operator bool
+        //------------------------------------------------------------------------------
         constexpr decltype(auto) operator*() { return *generator_; }
         constexpr GeneratorIterator& operator++() { ++generator_; return *this; }
         constexpr bool operator!=(const GeneratorEnd&) const { return generator_.operator bool(); }
 
     private:
+        //------------------------------------------------------------------------------
+        // Member variables
+        //------------------------------------------------------------------------------
         Generator& generator_;
     };
 }
