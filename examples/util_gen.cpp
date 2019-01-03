51d6fa1ed5e3300d0d0010885644e04d1962b3c2
Luke Allen <nightwolf55la@gmail.com>
2019-01-02 20:26:33 -0600

    Adding range examples

--- examples/util_gen.cpp
+++ examples/util_gen.cpp
@@ -7,16 +7,39 @@ int main() {
     std::cout << "Beginning examples: util::gen" << std::endl << std::endl;
 
     {
+        std::cout << "range" << std::endl;
+
+        std::cout << "Should print (0)(1)(2)(3)(4)" << std::endl << "             ";
+        for(int64_t value : util::gen::range(5)) {
+            std::cout << "(" << value << ")";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Should print (3)(4)(5)(6)" << std::endl << "             ";
+        for(int64_t value : util::gen::range(3,7)) {
+            std::cout << "(" << value << ")";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Should print (2)(5)(8)" << std::endl << "             ";
+        for(int64_t value : util::gen::range(2,11,3)) {
+            std::cout << "(" << value << ")";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+    {
         std::cout << "enumerate" << std::endl;
         std::vector<int> vec{1,2,3};
 
-        std::cout << "Should print (0,1)(1,2)(2,3):" << std::endl;
+        std::cout << "Should print (0,1)(1,2)(2,3)" << std::endl << "             ";
         for(auto&& [index, value] : util::gen::enumerate{vec}) {
             std::cout << "(" << index << "," << value << ")";
         }
         std::cout << std::endl;
 
-        std::cout << "Should print (10,1)(11,2)(12,3):" << std::endl;
+        std::cout << "Should print (10,1)(11,2)(12,3)" << std::endl << "             ";
         for(auto&& [index, value] : util::gen::enumerate{vec, 10}) {
             std::cout << "(" << index << "," << value << ")";
         }
@@ -25,11 +48,12 @@ int main() {
         auto get_vec = [](){
             return std::vector<int>{1,2,3};
         };
-        std::cout << "Should print (0,1)(1,2)(2,3):" << std::endl;
+        std::cout << "Should print (0,1)(1,2)(2,3)" << std::endl << "             ";
         for(auto&& [index, value] : util::gen::enumerate{get_vec()}) {
             std::cout << "(" << index << "," << value << ")";
         }
         std::cout << std::endl;
     }
+
     return 0;
 }
