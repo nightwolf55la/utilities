27679ea7e0e52f1597782e5d476e3d237e82a11c
Luke Allen <nightwolf55la@gmail.com>
2019-01-06 16:22:10 -0600

    Adding comments to examples

--- examples/util_gen.cpp
+++ examples/util_gen.cpp
@@ -7,21 +7,27 @@
 int main() {
     std::cout << "Beginning examples: util::gen" << std::endl << std::endl;
 
+    ////////////////////////////////////////////////////////////////////////////////
+    // range examples
+    ////////////////////////////////////////////////////////////////////////////////
     {
         std::cout << "range" << std::endl;
 
+        // Just using an end index
         std::cout << "Should print (0)(1)(2)(3)(4)" << std::endl << "             ";
         for(int64_t value : util::gen::range(5)) {
             std::cout << "(" << value << ")";
         }
         std::cout << std::endl;
 
+        // Using a beginning and end index
         std::cout << "Should print (3)(4)(5)(6)" << std::endl << "             ";
         for(int64_t value : util::gen::range(3,7)) {
             std::cout << "(" << value << ")";
         }
         std::cout << std::endl;
 
+        // Using a beginning and end index with a non default step
         std::cout << "Should print (2)(5)(8)" << std::endl << "             ";
         for(int64_t value : util::gen::range(2,11,3)) {
             std::cout << "(" << value << ")";
@@ -29,23 +35,29 @@ int main() {
         std::cout << std::endl;
     }
 
+    ////////////////////////////////////////////////////////////////////////////////
+    // enumerate examples
+    ////////////////////////////////////////////////////////////////////////////////
     std::cout << std::endl;
     {
         std::cout << "enumerate" << std::endl;
         std::vector<int> vec{1,2,3};
 
+        // Enumerate an lvalue reference starting with 0
         std::cout << "Should print (0,1)(1,2)(2,3)" << std::endl << "             ";
         for(auto&& [index, value] : util::gen::enumerate{vec}) {
             std::cout << "(" << index << "," << value << ")";
         }
         std::cout << std::endl;
 
+        // Enumerate an lvalue reference starting with 10
         std::cout << "Should print (10,1)(11,2)(12,3)" << std::endl << "             ";
         for(auto&& [index, value] : util::gen::enumerate{vec, 10}) {
             std::cout << "(" << index << "," << value << ")";
         }
         std::cout << std::endl;
 
+        // Enumerate an rvalue reference - this rvalue's lifetime is extended (no copies or moves performed)
         auto get_vec = [](){
             return std::vector<int>{1,2,3};
         };
@@ -56,6 +68,9 @@ int main() {
         std::cout << std::endl;
     }
 
+    ////////////////////////////////////////////////////////////////////////////////
+    // zip examples
+    ////////////////////////////////////////////////////////////////////////////////
     std::cout << std::endl;
     {
         std::cout << "zip" << std::endl;
@@ -64,30 +79,35 @@ int main() {
         std::list<double> list1{1.2, 3.4, 5.6};
         [[maybe_unused]] char arr[] = {'a', 'b', 'c'};
 
+        // Zip on just one iterable
         std::cout << "Should print (1)(2)(3)" << std::endl << "             ";
         for(auto&& [vec1_val] : util::gen::zip{vec1}) {
             std::cout << "(" << vec1_val << ")";
         }
         std::cout << std::endl;
 
+        // Zip on two iterables
         std::cout << "Should print (1,4)(2,5)(3,6)" << std::endl << "             ";
         for(auto&& [vec1_val, vec2_val] : util::gen::zip{vec1, vec2}) {
             std::cout << "(" << vec1_val << "," << vec2_val << ")";
         }
         std::cout << std::endl;
 
+        // Zip on three iterables
         std::cout << "Should print (1,4,1.2)(2,5,2.3)(3,6,4.5)" << std::endl << "             ";
         for(auto&& [vec1_val, vec2_val, list1_val] : util::gen::zip{vec1, vec2, list1}) {
             std::cout << "(" << vec1_val << "," << vec2_val << "," << list1_val << ")";
         }
         std::cout << std::endl;
 
+        // Zip on three iterables
         std::cout << "Should print (1,4,a)(2,5,b)(3,6,c)" << std::endl << "             ";
         for(auto&& [vec1_val, vec2_val, arr_val] : util::gen::zip{vec1, vec2, arr}) {
             std::cout << "(" << vec1_val << "," << vec2_val << "," << arr_val << ")";
         }
         std::cout << std::endl;
 
+        // Zip on three iterables with one being an rvalue - Rvalue's lifetime is extended (no copies or moves performed)
         auto get_vec = [](){
             return std::vector<int>{7,8,9};
         };
