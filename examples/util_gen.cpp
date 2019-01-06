0a91b35ecba7182290fe59562255c31a39d59e55
Luke Allen <nightwolf55la@gmail.com>
2019-01-06 15:04:37 -0600

    Adding zip examples

--- examples/util_gen.cpp
+++ examples/util_gen.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
 #include <util/generator.h>
 
@@ -51,6 +52,48 @@ int main() {
         std::cout << "Should print (0,1)(1,2)(2,3)" << std::endl << "             ";
         for(auto&& [index, value] : util::gen::enumerate{get_vec()}) {
             std::cout << "(" << index << "," << value << ")";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+    {
+        std::cout << "zip" << std::endl;
+        std::vector<int> vec1{1,2,3};
+        std::vector<int> vec2{4,5,6};
+        std::list<double> list1{1.2, 3.4, 5.6};
+        [[maybe_unused]] char arr[] = {'a', 'b', 'c'};
+
+        std::cout << "Should print (1)(2)(3)" << std::endl << "             ";
+        for(auto&& [vec1_val] : util::gen::zip{vec1}) {
+            std::cout << "(" << vec1_val << ")";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Should print (1,4)(2,5)(3,6)" << std::endl << "             ";
+        for(auto&& [vec1_val, vec2_val] : util::gen::zip{vec1, vec2}) {
+            std::cout << "(" << vec1_val << "," << vec2_val << ")";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Should print (1,4,1.2)(2,5,2.3)(3,6,4.5)" << std::endl << "             ";
+        for(auto&& [vec1_val, vec2_val, list1_val] : util::gen::zip{vec1, vec2, list1}) {
+            std::cout << "(" << vec1_val << "," << vec2_val << "," << list1_val << ")";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Should print (1,4,a)(2,5,b)(3,6,c)" << std::endl << "             ";
+        for(auto&& [vec1_val, vec2_val, arr_val] : util::gen::zip{vec1, vec2, arr}) {
+            std::cout << "(" << vec1_val << "," << vec2_val << "," << arr_val << ")";
+        }
+        std::cout << std::endl;
+
+        auto get_vec = [](){
+            return std::vector<int>{7,8,9};
+        };
+        std::cout << "Should print (1,7,a)(2,8,b)(3,9,c)" << std::endl << "             ";
+        for(auto&& [vec1_val, vec2_val, arr_val] : util::gen::zip{vec1, get_vec(), arr}) {
+            std::cout << "(" << vec1_val << "," << vec2_val << "," << arr_val << ")";
         }
         std::cout << std::endl;
     }
--- include/util/generator.h
+++ include/util/generator.h
@@ -3,3 +3,4 @@
 #include "gen/enumerate.h"
 #include "gen/generator.h"
 #include "gen/range.h"
+#include "gen/zip.h"
